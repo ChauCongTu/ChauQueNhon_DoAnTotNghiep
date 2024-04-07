c00b98c0ace8e8519b64bc91268fb077b9d50e7b
NhonCQ <quenhon.dev@gmail.com>
2024-04-07 19:47:33 +0700

    optimize skeleton

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,7 +6,8 @@ use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 Route::get('/', function () {
-    return view('welcome');
+    $message = 'Bạn không có quyền truy cập vào nguồn dữ liệu đã yêu cầu.';
+    return Common::response(403, $message);
 });
 Route::get('/response-403', function(){
     $message = 'Bạn không có quyền truy cập vào nguồn dữ liệu đã yêu cầu.';
